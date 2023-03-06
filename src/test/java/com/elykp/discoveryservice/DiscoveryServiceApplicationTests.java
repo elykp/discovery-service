d809f02348848b328d63135828de4d48d76b9651
IDEALOGIC\khoa-dang.pham <khoa.pham@publishone.nl>
2023-03-06 11:00:09 +0700

    feat: init discovery service

---
+++ src/test/java/com/elykp/discoveryservice/DiscoveryServiceApplicationTests.java
@@ -0,0 +1,15 @@
+package com.elykp.discoveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootTest
+class DiscoveryServiceApplicationTests {
+
+	@Test
+	void contextLoads(ApplicationContext context) {
+		assert context != null;
+	}
+
+}
